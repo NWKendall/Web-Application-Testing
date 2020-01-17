df1fe44e00d8891a665fdf86f3172dc3c6ff6c4b
nicbongo <nicbongo@protonmail.com>
2020-01-16 20:09:19 -0400

    MVP

--- scoreboard/src/App.js
+++ scoreboard/src/App.js
@@ -1,12 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import DashBoard from "./components/Dashboard/Dashboard"
-import { Count } from "./components/Scores"
-
 
 function App() {
-
-  
   return (
     <div className="App">
       <DashBoard />
--- scoreboard/src/components/Dashboard/Dashboard.js
+++ scoreboard/src/components/Dashboard/Dashboard.js
@@ -9,18 +9,17 @@ const DashBoard = () => {
         [ball, setBall]     = Count();
   
   if(strike === 3) {
-    setStrike(0)
+    setStrike(0);
     setBall(0);
   }
 
   if(ball === 4){
-    setStrike(0)
+    setStrike(0);
     setBall(0);
   }
 
-  console.log(strike)
-  console.log(ball)
-
+  const foul = () => strike <= 2 ? setStrike(2) : strike;
+  
   return (
     <div>
       <Display strike={strike} ball={ball} />
@@ -28,11 +27,11 @@ const DashBoard = () => {
       <h3>DashBoard</h3>
       <div>
         <button onClick={() => setBall(ball+1)}>Ball</button>
-        <button onClick={""}>Hit</button>
+        <button onClick={() => setBall(0) + setStrike(0)}>Hit</button>
       </div>
       <div>
         <button onClick={() => setStrike(strike+1)}>Strike</button>
-        <button onClick={""}>Foul</button>
+        <button onClick={foul}>Foul</button>
       </div>
       
 
